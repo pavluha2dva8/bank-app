8e5ddb3c7f11c75982ec3d67b3c882cc9e5a3c3c
pavluha2dva8 <pavluha2dva8@gmail.com>
2021-03-25 14:12:27 +0200

    refactor reducers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import './App.css';
+import {addClientActionCreator, removeClientActionCreator} from "./store/clientsReducer";
+import {addCashActionCreator, getCashActionCreator} from "./store/cashReducer";
+import {Button} from "@material-ui/core";
 
 function App() {
-  const dispatch = useDispatch()
-  const cash = useSelector( state => state.cash) // function that takes state and return smth
+    const dispatch = useDispatch()
+    const cash = useSelector(state => state.cash.cash) // function that takes state and return smth
+    const clients = useSelector(state => state.clients.clients)
 
-  let addCash = (cash) => {
-    dispatch({type: 'ADD_CASH', payload: cash})
-  }
-  let getCash = (cash) => {
-    dispatch({type: 'GET_CASH', payload: cash})
-  }
+    let addCash = (cash) => {
+        isNaN(cash) ? alert('Input must be a number') : dispatch(addCashActionCreator(cash))
+    }
+    let getCash = (cash) => {
+        isNaN(cash) ? alert('Input must be a number') : dispatch(getCashActionCreator(cash))
+    }
+    let addClient = (name) => {
+        let client = {
+            name,
+            id: Date.now()
+        }
+        dispatch(addClientActionCreator(client))
+    }
+    let removeClient = (client) => {
+        dispatch(removeClientActionCreator(client.id))
+    }
 
-  return (
-      <div className="App">
-        <h1>bank-app</h1>
-        <div className="App-output">
-          {cash}
+    return (
+        <div className="App">
+            <h1>bank-app</h1>
+            <div className="App-output">
+                {cash}
+            </div>
+            <Button variant="contained" color="primary"
+                    onClick={() => addCash(Number(prompt()))}>ADD_CASH
+            </Button>
+            <Button variant="contained" color="primary"
+                    onClick={() => getCash(Number(prompt()))}>GET_CASH
+            </Button>
+            <Button variant="contained" color="primary" onClick={() => addClient(prompt())}
+            >ADD_CLIENT
+            </Button>
+            {clients.length > 0 ?
+                <div>
+                    {clients.map(client =>
+                        <div key={client.id} onClick={() => removeClient(client)}>{client.name}</div>
+                    )}
+                </div>
+                :
+                <div>no clients</div>}
         </div>
-        <button onClick={ () => addCash(Number(prompt()))}>ADD_CASH</button>
-        <button onClick={ () => getCash(Number(prompt()))}>GET_CASH</button>
-      </div>
-  );
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,26 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-const initialState = {
-    cash: 0
-}
-
-const cashReduser = (state = initialState, action) => {
-    switch (action.type) {
-        case 'ADD_CASH':
-            return {...state, cash: state.cash + action.payload}
-        case 'GET_CASH':
-            return {...state, cash: state.cash - action.payload}
-
-        default:
-            return state
-    }
-}
-
-const store = createStore(cashReduser)
+import {store} from "./store";
 
 ReactDOM.render(
     <Provider store={store}>
